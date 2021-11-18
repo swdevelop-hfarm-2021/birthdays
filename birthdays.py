671515c95e2682b9e637a5f2261136740d236b52
Massimo <massimo.sirianni@student.h-farm.com>
2021-11-18 15:53:30 +0100

    compleanno

--- birthdays.py
+++ birthdays.py
@@ -3,7 +3,9 @@ birthdays = {
     'Benjamin Franklin': '01/17/1706',
     'Ada Lovelace': '12/10/1815',
     'Donald Trump': '06/14/1946',
-    'Rowan Atkinson': '01/6/1955'}
+    'Rowan Atkinson': '01/6/1955'
+    'Massimo XXXXX': '02/12/1998'
+}
 
 def print_birthdays():
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
