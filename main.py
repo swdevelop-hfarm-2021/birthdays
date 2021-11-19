96d961f7cb2b991664e69631876899362624791f
marijasole <mariasolelanza@gmail.com>
2021-11-19 15:33:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from birthdays import return_birthday
 
 return_birthday('Albert Einstein')
 return_birthday('Alan Turing')
+<<<<<<< HEAD
 return_birthday('Edoardo Sozzi')
 #ciau
 =======
+return_birthday('Edoardo Sozzi') #bbbb
 >>>>>>> 527a0f17d55819315cabc08fb4e68c71a710627d
