d3de17029857d3eb9aa46236d79932c3f8bd5f17
AnnalisaLaini <annalisa.laini@student.h-farm.com>
2021-11-22 04:31:47 -0500

    documented code

--- birthdays.py
+++ birthdays.py
@@ -1,3 +1,10 @@
+'''
+Hello World! This module allows you to check the birthday of some relevant figures in our world's history.
+
+'''
+
+
+
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
@@ -6,11 +13,17 @@ birthdays = {
     'Rowan Atkinson': '01/6/1955'}
 
 def print_birthdays():
+'''
+this first function allows you to check who's in the list of birthdays
+'''
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
     for name in birthdays:
         print(name)
 
 def return_birthday(name):
+'''
+our second function is going to print the birthday date of the person if she/he is in the list, otherwise it will print an error message saying we don't have the information required
+'''
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
